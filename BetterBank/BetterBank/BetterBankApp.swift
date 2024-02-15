3b91118fc077bbdb0e78d673a17850d2a587c547
LenZuro <126126939+LenZuro@users.noreply.github.com>
2024-02-15 15:04:57 -0800

    Inital commit

---
+++ BetterBank/BetterBank/BetterBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterBankApp.swift
+//  BetterBank
+//
+//  Created by Len Zuro on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
